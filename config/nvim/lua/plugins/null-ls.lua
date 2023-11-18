7e64d57943cc23b191789962975ab28647e0bd86
yoelvp <yoelvp73@gmail.com>
2023-11-17 20:36:23 -0500

    remove formatting

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -49,22 +49,22 @@ return {
     end
 
     -- if you want to set up formatting on save, you can use this as a callback
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+    -- local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
-    local on_attach = function(client, bufnr)
-      if client.supports_method('textDocument/formatting') then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd('BufWritePre', {
-          group = augroup,
-          buffer = bufnr,
-          callback = function() lsp_formatting(bufnr) end
-        })
-      end
-    end
+    -- local on_attach = function(client, bufnr)
+    --   if client.supports_method('textDocument/formatting') then
+    --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    --     vim.api.nvim_create_autocmd('BufWritePre', {
+    --       group = augroup,
+    --       buffer = bufnr,
+    --       callback = function() lsp_formatting(bufnr) end
+    --     })
+    --   end
+    -- end
 
     null_ls.setup({
-      sources = sources,
-      on_attach = on_attach
+      sources = sources
+      -- on_attach = on_attach
     })
   end
 }
