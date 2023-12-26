c3d8dcfe58163b9accc5a5599563f7dddd8e939b
yoelvp <yoelvp73@gmail.com>
2023-12-26 17:40:27 -0500

    modify: modify keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- disable macros
@@ -32,11 +32,14 @@ keymap('n', 'sv', ':vsplit<CR> :wincmd l<CR>', opts)
 -- keymap('n', 'st', ':vsplit<CR><C-w>w', opts)
 
 -- Format code
-keymap('n', '<leader>ff', ':lua vim.lsp.buf.format({ async = true })<CR>', opts)
+keymap('n', '<leader>ff', ':lua vim.lsp.buf.format({ async = true })<CR>', opts, { desc = 'Format document'})
+
+-- Recharge file
+keymap('n', '<leader>rl', ':luafi %<CR>', opts, { desc = 'Realod file config' })
 
 -- Keymaps LSP
-keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', opts)
-keymap('n', 'rn', ':lua vim.lsp.buf.rename()<CR>', opts)
+keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', opts, { desc = 'Go to definition'})
+keymap('n', 'rn', ':lua vim.lsp.buf.rename()<CR>', opts, { desc = 'Rename variables'})
 keymap('n', 'H', ':lua vim.lsp.buf.hover()<CR>', opts)
 keymap('n', 'K', ':lua vim.lsp.buf.signature_help()<CR>', opts)
 keymap('n', '<C-j>', ':lua vim.diagnostic.goto_next()<CR>', opts)
@@ -45,18 +48,22 @@ keymap('n', '<leader>ge', ':lua vim.diagnostic.open_float()<CR>', opts)
 -- keymap('n', '<leader>gl', ':lua vim.diagnostic.setloclist()<CR>', opts)
 
 -- Move next buffer
-keymap('n', '<Tab>', ':bnext<CR>', opts)
-keymap('n', '<S-Tab>', ':bprevious<CR>', opts)
+keymap('n', '<Tab>', ':bnext<CR>', opts, { desc = 'Move to next buffer' })
+keymap('n', '<S-Tab>', ':bprevious<CR>', opts, { desc = 'Move to previous buffer' })
 keymap('n', '<C-w>', ':bdelete<CR>', opts)
 
 -- Move line up and down
-keymap('n', '<A-j>', '<cmd>m .+1<cr>==', { noremap = true, silent = true, desc = 'Move down' })
-keymap('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
-keymap('v', '<A-j>', "<cmd>m '>+1<cr>gv=gv", { desc = 'Move down' })
-keymap('v', '<A-k>', "<cmd>m '<-2<cr>gv=gv", { desc = 'Move up' })
+keymap('n', '<A-j>', '<cmd>m .+1<cr>==', opts, { desc = 'Move down' })
+keymap('n', '<A-k>', '<cmd>m .-2<cr>==', opts, { desc = 'Move up' })
+keymap('v', '<A-j>', "<cmd>m '>+1<cr>gv=gv", opts, { desc = 'Move down' })
+keymap('v', '<A-k>', "<cmd>m '<-2<cr>gv=gv", opts, { desc = 'Move up' })
 
 -- Open lazygit
-keymap('n', '<leader>lg', ':LazyGit<CR>', { noremap = true, silent = true })
+keymap('n', '<leader>lg', ':LazyGit<CR>', opts, { desc = 'Open lazygit' })
 
 -- Neotree
-keymap('n', 'nf', ':Neotree toggle<CR>', opts)
+keymap('n', 'nf', ':Neotree toggle<CR>', opts, { desc = 'Toggle neotree'})
+
+-- Compile and run rust projects
+keymap('n', '<leader>rmk', '[[:w<CR>:term cargo run<CR>]]', opts, { desc = 'Compile rust projects' })
+keymap('n', '<leader>cmk', '[[:w<CR>:term gcc main.c -o main && ./main<CR>]]', opts, { desc = 'Compile rust projects' })
