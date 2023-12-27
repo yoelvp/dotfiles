be5061ae74257b93b280e18d1c335c3ed9285b10
yoelvp <yoelvp73@gmail.com>
2023-12-27 13:55:16 -0500

    remove plugins

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,14 @@
 return {
   'nvim-telescope/telescope.nvim',
+  event = 'VeryLazy',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-lua/popup.nvim',
-    'nvim-telescope/telescope-fzy-native.nvim',
     'nvim-tree/nvim-web-devicons',
     'nvim-telescope/telescope-file-browser.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
-    'nvim-telescope/telescope-fzf-native.nvim',
     'nvim-telescope/telescope-media-files.nvim'
   },
-  event = 'VeryLazy',
   config = function()
     require('yoelvp.telescope')
   end
