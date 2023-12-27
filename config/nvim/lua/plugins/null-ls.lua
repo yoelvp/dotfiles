b7519c436d360fe794bbe114dc76164e1c863af8
yoelvp <yoelvp73@gmail.com>
2023-12-27 13:55:07 -0500

    refactor

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -26,7 +26,6 @@ return {
         end
       }),
       diagnostics.shellcheck,
-      -- diagnostics.phpcs,
 
       code_actions.eslint_d.with({
         condition = function(utils)
@@ -36,30 +35,8 @@ return {
       code_actions.shellcheck,
     }
 
-    -- local lsp_formatting = function(bufnr)
-    --   vim.lsp.buf.format({
-    --     filter = function(client) return client.name == 'null-ls' end,
-    --     bufnr = bufnr
-    --   })
-    -- end
-
-    -- if you want to set up formatting on save, you can use this as a callback
-    -- local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-
-    -- local on_attach = function(client, bufnr)
-    --   if client.supports_method('textDocument/formatting') then
-    --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    --     vim.api.nvim_create_autocmd('BufWritePre', {
-    --       group = augroup,
-    --       buffer = bufnr,
-    --       callback = function() lsp_formatting(bufnr) end
-    --     })
-    --   end
-    -- end
-
     null_ls.setup({
       sources = sources
-      -- on_attach = on_attach
     })
   end
 }
