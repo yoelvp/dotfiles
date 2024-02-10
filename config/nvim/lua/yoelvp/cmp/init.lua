e3f9a0ae56e2c4b91b20b47e129038b9cf26d55a
yoelvp <yoelvp73@gmail.com>
2024-02-09 22:17:08 -0500

    fix: autopairs with completion

--- config/nvim/lua/yoelvp/cmp/init.lua
+++ config/nvim/lua/yoelvp/cmp/init.lua
@@ -3,7 +3,6 @@ local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 local autopairs = require('nvim-autopairs.completion.cmp')
 
--- cmp.event:on('confirm_done', autopairs.on_confirm_done({ map_char = { tex = '' } }))
 cmp.event:on('confirm_done', autopairs.on_confirm_done())
 
 cmp.setup({
