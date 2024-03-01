c333ecbb2141f2d0605aaae44d6fe8b786f87f5f
yoelvp <yoelvp73@gmail.com>
2024-02-29 23:30:30 -0500

    fix: change config

--- config/nvim/lua/yoelvp/neotree/init.lua
+++ config/nvim/lua/yoelvp/neotree/init.lua
@@ -1,8 +1,4 @@
-local ok, neotree = pcall(require, 'neo-tree')
-
-if not ok then
-  return
-end
+local neotree = require('neo-tree')
 
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
@@ -13,20 +9,19 @@ vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSi
 vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
 
 neotree.setup({
-  close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+  close_if_last_window = true,
   popup_border_style = 'rounded',
   enable_git_status = true,
   enable_diagnostics = true,
-  open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
-  sort_case_insensitive = false, -- used when sorting files and directories in the tree
-  sort_function = nil, -- use a custom function for sorting files and directories in the tree
+  sort_case_insensitive = false,
+  sort_function = nil,
   default_component_configs = {
     container = {
       enable_character_fade = true,
     },
     indent = {
       indent_size = 2,
-      padding = 1, -- extra padding on left hand side
+      padding = 1,
       with_markers = true,
       indent_marker = '│',
       last_indent_marker = '└',
@@ -66,7 +61,6 @@ neotree.setup({
       },
     },
   },
-  commands = {},
   window = {
     position = 'right',
     width = 40,
@@ -75,11 +69,6 @@ neotree.setup({
       nowait = true,
     },
     mappings = {
-      ['<space>'] = {
-        'toggle_node',
-        nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
-      },
-      -- ['<2-LeftMouse>'] = 'open',
       ['<cr>'] = 'open',
       ['<esc>'] = 'revert_preview',
       ['P'] = { 'toggle_preview', config = { use_float = true } },
@@ -88,7 +77,6 @@ neotree.setup({
       ['s'] = 'open_vsplit',
       ['t'] = 'open_tabnew',
       ['w'] = 'open',
-      -- ['w'] = 'open_with_window_picker',
       ['h'] = 'close_node',
       ['l'] = 'open',
       ['z'] = 'close_all_nodes',
@@ -113,7 +101,6 @@ neotree.setup({
       ['>'] = 'next_source',
     },
   },
-  nesting_rules = {},
   filesystem = {
     filtered_items = {
       visible = false, -- when true, they will just be displayed differently than normal items
@@ -124,25 +111,15 @@ neotree.setup({
         'node_modules',
         '.git',
       },
-      hide_by_pattern = { -- uses glob style patterns
-        --"*.meta",
-        --"*/src/*/tsconfig.json",
-      },
-      always_show = { -- remains visible even if other settings would normally hide it
-        --".gitignored",
-      },
-      never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+      never_show = {
         ".DS_Store",
         "thumbs.db"
       },
-      never_show_by_pattern = { -- uses glob style patterns
-        --".null-ls_*",
-      },
     },
-    follow_current_file = false, -- This will find and focus the file in the active buffer every
+    follow_current_file = true, -- This will find and focus the file in the active buffer every
     group_empty_dirs = false, -- when true, empty folders will be grouped together
     hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
-    use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+    use_libuv_file_watcher = false,
     window = {
       mappings = {
         ['<bs>'] = 'navigate_up',
@@ -164,8 +141,15 @@ neotree.setup({
         ['<C-p>'] = 'move_cursor_up',
       },
     },
-
-    commands = {}, -- Add a custom command or override a global one using the same function name
+  },
+  event_handlers = {
+    {
+      event = "neo_tree_buffer_enter",
+      handler = function()
+        vim.opt_local.number = true
+        vim.opt_local.relativenumber = true
+      end
+    }
   },
   buffers = {
     follow_current_file = true, -- This will find and focus the file in the active buffer every
@@ -176,20 +160,6 @@ neotree.setup({
         ['bd'] = 'buffer_delete',
         ['<bs>'] = 'navigate_up',
         ['.'] = 'set_root',
-      },
-    },
-  },
-  git_status = {
-    window = {
-      position = 'float',
-      mappings = {
-        ['A'] = 'git_add_all',
-        ['gu'] = 'git_unstage_file',
-        ['ga'] = 'git_add_file',
-        ['gr'] = 'git_revert_file',
-        ['gc'] = 'git_commit',
-        ['gp'] = 'git_push',
-        ['gg'] = 'git_commit_and_push',
       },
     },
   },
