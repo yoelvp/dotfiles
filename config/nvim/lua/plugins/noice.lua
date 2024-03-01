9cc6be134ecad4f44a336c6c3083768d1ae77d04
yoelvp <yoelvp73@gmail.com>
2024-02-29 23:33:14 -0500

    remove comments

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -13,22 +13,22 @@ return {
     },
     -- -- you can enable a preset for easier configuration
     presets = {
-      bottom_search = true,         -- use a classic bottom cmdline for search
+      bottom_search = true, -- use a classic bottom cmdline for search
       --   command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       -- inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true,        -- add a border to hover docs and signature help
+      lsp_doc_border = true, -- add a border to hover docs and signature help
     },
     cmdline = {
       view = "cmdline",
     },
     messages = {
-      enabled = true,            -- enables the Noice messages UI
-      view = "notify",           -- default view for messages
-      view_error = "notify",     -- view for errors
-      view_warn = "notify",      -- view for warnings
+      enabled = true, -- enables the Noice messages UI
+      view = "notify", -- default view for messages
+      view_error = "notify", -- view for errors
+      view_warn = "notify", -- view for warnings
       view_history = "messages", -- view for :messages
-      view_search = false,       -- view for search count messages. Set to `false` to disable
+      view_search = false, -- view for search count messages. Set to `false` to disable
     },
     routes = {
       {
@@ -106,5 +106,5 @@ return {
         },
       },
     },
-  }
+  },
 }
