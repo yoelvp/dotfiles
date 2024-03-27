b84b35f1fdfd594fa6e954352b62f10f35665d96
yoelvp <yoelvp73@gmail.com>
2024-03-26 21:58:01 -0500

    fix: add custom snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,10 +3,12 @@ return {
   version = false,
   event = 'InsertEnter',
   dependencies = {
+    'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-path',
     'saadparwaiz1/cmp_luasnip',
+    'windwp/nvim-autopairs',
     {
       'L3MON4D3/LuaSnip',
       version = 'v2.*',
@@ -17,6 +19,7 @@ return {
   --- @param opts cmp.ConfigSchema
   config = function()
     require('yoelvp.cmp')
-    require('luasnip.loaders.from_vscode').lazy_load()
+    -- require('luasnip.loaders.from_vscode').lazy_load()
+    require('luasnip.loaders.from_vscode').lazy_load({ paths = { '/home/yoelvp/dotfiles/nvim/snippets' } })
   end,
 }
