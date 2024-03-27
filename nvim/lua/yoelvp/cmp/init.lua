fa9537dce14501613068a9c3036cc8598154cb4c
yoelvp <yoelvp73@gmail.com>
2024-03-26 22:02:35 -0500

    fix: add new config and sources for completion

--- nvim/lua/yoelvp/cmp/init.lua
+++ nvim/lua/yoelvp/cmp/init.lua
@@ -3,7 +3,7 @@ local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 local autopairs = require('nvim-autopairs.completion.cmp')
 
-cmp.event:on('confirm_done', autopairs.on_confirm_done())
+cmp.event:on('confirm_done', autopairs.on_confirm_done({ map_char = { tex = '' } }))
 vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
 
 cmp.setup({
@@ -46,7 +46,23 @@ cmp.setup({
     { name = 'nvim_lua' },
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    { name = 'path' }
+    { name = 'path' },
+    {
+      name = "buffer",
+      keyword_length = 4,
+      option = {
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local bufnr = vim.api.nvim_win_get_buf(win)
+            if vim.api.nvim_buf_get_option(bufnr, 'buftype') ~= 'terminal' then
+              bufs[bufnr] = true
+            end
+          end
+          return vim.tbl_keys(bufs)
+        end,
+      },
+    },
   }),
   formatting = {
     format = lspkind.cmp_format({
@@ -72,3 +88,11 @@ vim.cmd([[
   set completeopt=menuone,noinsert,noselect
   highlight! default link CmpItemKind CmpItemMenuDefault
 ]])
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
