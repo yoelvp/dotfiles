c01651cfe7c7a2d520bed3446918716e0a0b0d1a
yoelvp <yoelvp73@gmail.com>
2024-03-28 11:23:57 -0500

    fix: add utils in init file

--- nvim/lua/yoelvp/utils/init.lua
+++ nvim/lua/yoelvp/utils/init.lua
@@ -1,11 +1,23 @@
 local M = {}
 
-M.ui = require('yoelvp.utils.ui')
 M.neotree = require('yoelvp.utils.neotree')
 M.react = require('yoelvp.utils.react')
 
+-- Extend table
 function M.extendTable(table, values)
   return vim.tbl_extend('force', table, values)
+end
+
+-- Duplicate current line below
+function M.duplicate_line_below()
+  vim.api.nvim_command('normal! yyp')
+  vim.api.nvim_command('noh')
+end
+
+-- Duplicate current line above
+function M.duplicate_line_above()
+  vim.api.nvim_command('normal! yyP')
+  vim.api.nvim_command('noh')
 end
 
 return M
