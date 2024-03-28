660c18630f7510da74551f7e72f1c89622f643ca
yoelvp <yoelvp73@gmail.com>
2024-03-28 11:22:18 -0500

    feat: add my custom colorschemes

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,119 @@
+return {
+  -- Gruvbox
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      require('gruvbox').setup({
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = false,
+          comments = true,
+          operators = false,
+          folds = true
+        },
+        strikethrough = true,
+        invert_selection = true,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = true,
+        inverse = true,
+        contrast = 'hard',
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false
+      })
+    end
+  },
+
+  -- Onedark
+  {
+    'olimorris/onedarkpro.nvim',
+    priority = 1000,
+    config = function ()
+      local onedark = require('onedarkpro')
+
+      onedark.setup({
+        styles = {
+          types = 'NONE',
+          methods = 'NONE',
+          numbers = 'NONE',
+          strings = 'NONE',
+          comments = 'italic',
+          keywords = 'bold,italic',
+          constants = 'NONE',
+          functions = 'italic',
+          operators = 'NONE',
+          variables = 'NONE',
+          parameters = 'NONE',
+          conditionals = 'italic',
+          virtual_text = 'italic'
+        },
+        highlights = {
+          Comment = { italic = true },
+        },
+        options = {
+          lualine_transparency = true
+        }
+      })
+    end
+  },
+
+  -- Sonokai
+  {
+    'sainnhe/sonokai',
+    priority = 1000,
+    config = true
+  },
+
+  -- Everforest
+  {
+    'sainnhe/everforest',
+    priority = 1000,
+    config = true
+  },
+
+  -- Ayu Theme
+  {
+    'comfysage/evergarden',
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      contrast_dark = 'medium', -- 'hard'|'medium'|'soft'
+      style = {
+        tabline = { reverse = true, color = 'green' },
+        search = { reverse = false, inc_reverse = true },
+        types = { italic = true },
+        keyword = { italic = true },
+        comment = { italic = true },
+      }
+    },
+    config = function (_, opts)
+      require('evergarden').setup(opts)
+    end
+  },
+
+  -- Solarized Osaka
+  {
+    'craftzdog/solarized-osaka.nvim',
+    priority = 1000,
+    config = function ()
+      local solarized = require('solarized-osaka')
+
+      solarized.setup({
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          sidebars = 'dark',
+          floats = 'dark',
+        },
+        sidebars = { 'qf', 'help' }
+      })
+      end
+  }
+}
