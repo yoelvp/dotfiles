9770a6366d7b8f074888fa9dfb9ca148b1d336f7
yoelvp <yoelvp73@gmail.com>
2024-03-28 11:22:06 -0500

    fix: remove nvim-colorizer

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,18 +1,8 @@
 return {
-  'norcalli/nvim-colorizer.lua',
-  event = 'VeryLazy',
-  config = function()
-    local colorizer = require('colorizer')
-
-    colorizer.setup({
-      '*',
-      'markdown',
-      'yaml',
-      css = { rgb_fn = true, names = true },
-      yaml = { rgb_fn = true },
-      html = { names = true }
-    }, {
-      mode = 'background'
-    })
-  end
+  'NvChad/nvim-colorizer.lua',
+  opts = {
+    user_default_options = {
+      tailwind = true
+    }
+  }
 }
