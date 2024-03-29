999b83a5e4dcd353845051f434eed04207ed2f59
yoelvp <yoelvp73@gmail.com>
2024-03-29 00:17:23 -0500

    fix: comment fzf extension

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,7 +7,11 @@ return {
     'nvim-tree/nvim-web-devicons',
     'nvim-telescope/telescope-file-browser.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
-    'nvim-telescope/telescope-media-files.nvim'
+    'nvim-telescope/telescope-media-files.nvim',
+    --[[ { ]]
+    --[[   'nvim-telescope/telescope-fzf-native.nvim', ]]
+    --[[   build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' ]]
+    --[[ } ]]
   },
   config = function()
     require('yoelvp.telescope')
