9e502d2aae98390bf8db0ffbb466385962589ba1
yoelvp <yoelvp73@gmail.com>
2024-03-30 02:18:25 -0500

    fix: remove unnecesary plugins

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -16,8 +16,7 @@ return {
     {
       'L3MON4D3/LuaSnip',
       version = 'v2.*',
-      build = 'make install_jsregexp',
-      dependencies = 'rafamadriz/friendly-snippets'
+      build = 'make install_jsregexp'
     }
   },
   opts = function (_, opts)
@@ -27,7 +26,6 @@ return {
   end,
   config = function()
     require('yoelvp.cmp')
-    -- require('luasnip.loaders.from_vscode').lazy_load()
     require('luasnip.loaders.from_vscode').lazy_load({ paths = { '/home/yoelvp/dotfiles/nvim/snippets' } })
   end,
 }
