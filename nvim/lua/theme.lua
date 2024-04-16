bb479f6650717c632a42665ff3f0a3498e043ec4
yoelvp <yoelvp73@gmail.com>
2024-04-16 11:38:01 -0500

    fix: change of theme

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -2,7 +2,7 @@
 -- This block will be replaced by Themery.
 vim.cmd("colorscheme everforest")
 
-vim.cmd('let g:everforest_background = "medium"')
+vim.cmd('let g:everforest_background = "hard"')
 
-vim.g.theme_id = 2
+vim.g.theme_id = 1
 -- end themery block
