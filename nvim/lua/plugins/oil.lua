d1076ced14051b9f182234d4b57bff4f1b4a9aea
yoelvp <yoelvp73@gmail.com>
2024-04-15 21:15:16 -0500

    fix: remove config

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,8 +2,6 @@ return {
   'stevearc/oil.nvim',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('oil').setup({
-      default_file_explorer = true,
-    })
+    require('oil').setup({})
   end,
 }
