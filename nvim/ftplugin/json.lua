3ee4a8ce11792982871cd02dc575e1ea8255ad21
yoelvp <yoelvp73@gmail.com>
2024-04-15 21:05:02 -0500

    fix: rename config

---
+++ nvim/ftplugin/json.lua
@@ -0,0 +1,6 @@
+local opt = vim.opt_local
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth =  2
---
+++ nvim/ftplugin/yaml.lua
@@ -0,0 +1,7 @@
+local opt = vim.opt_local
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.indentkeys = '-=<:>'
