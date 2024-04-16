eb44af4f04f5338ecd06e8ac3f0c0a74e41be192
yoelvp <yoelvp73@gmail.com>
2024-04-15 21:04:51 -0500

    fix: move null configs

---
+++ nvim/backup/null-ls.lua
@@ -0,0 +1,31 @@
+return {
+  'jose-elias-alvarez/null-ls.nvim',
+  dependencies = 'nvim-lua/plenary.nvim',
+  config = function()
+    local null_ls = require('null-ls')
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+    local code_actions = null_ls.builtins.code_actions
+
+    local sources = {
+      -- Code actions
+      code_actions.eslint_d,
+      code_actions.shellcheck,
+
+      -- Diagnostics
+      diagnostics.eslint_d,
+      diagnostics.shellcheck,
+
+      -- Formatting
+      formatting.eslint_d,
+      formatting.stylua,
+      formatting.stylelint
+      -- formatting.gofumpt,
+      -- formatting.goimports_reviser
+    }
+
+    null_ls.setup({
+      sources = sources
+    })
+  end
+}
