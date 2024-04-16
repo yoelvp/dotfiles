332e222d3fea17c247822f9498a3898907c42b28
yoelvp <yoelvp73@gmail.com>
2024-04-15 21:11:54 -0500

    fix: remove unnecesary plugins/dependencies

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,27 +3,17 @@ return {
   version = false,
   event = 'InsertEnter',
   dependencies = {
-    'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lua',
     'hrsh7th/cmp-path',
     'saadparwaiz1/cmp_luasnip',
     'windwp/nvim-autopairs',
     {
-      'roobert/tailwindcss-colorizer-cmp.nvim',
-      config = true,
-    },
-    {
       'L3MON4D3/LuaSnip',
       version = 'v2.*',
       build = 'make install_jsregexp',
     },
   },
-  opts = function(_, opts)
-    opts.formatting = {
-      format = require('tailwindcss-colorizer-cmp').formatter,
-    }
-  end,
   config = function()
     require('yoelvp.cmp')
     require('luasnip.loaders.from_vscode').lazy_load({ paths = { '/home/yoelvp/dotfiles/nvim/snippets' } })
