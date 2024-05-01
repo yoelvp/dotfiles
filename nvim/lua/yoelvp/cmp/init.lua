35de0e8a9137c33ce3c3af389922734750cef074
yoelvp <yoelvp73@gmail.com>
2024-04-30 21:17:52 -0500

    fix: remove tailwind format

--- nvim/lua/yoelvp/cmp/init.lua
+++ nvim/lua/yoelvp/cmp/init.lua
@@ -3,7 +3,6 @@ local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 local utils = require('yoelvp.utils')
 local autopairs = require('nvim-autopairs.completion.cmp')
-
 cmp.event:on('confirm_done', autopairs.on_confirm_done({ map_char = { tex = '' } }))
 vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
 
@@ -22,11 +21,9 @@ cmp.setup({
   },
   window = {
     completion = {
-      border = 'rounded',
       scrollbar = true,
     },
     documentation = {
-      border = 'rounded',
       scrollbar = true,
     },
   },
@@ -77,29 +74,7 @@ cmp.setup({
         buffer = '[BUFFER]',
       },
       before = function(entry, vim_item)
-        if vim_item.kind == 'Color' and entry.completion_item.documentation then
-          local _, _, r, g, b =
-            ---@diagnostic disable-next-line: param-type-mismatch
-            string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
-
-          if r and g and b then
-            local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
-            local group = 'Tw_' .. color
-
-            if vim.api.nvim_call_function('hlID', { group }) < 1 then
-              vim.api.nvim_command('highlight' .. ' ' .. group .. ' ' .. 'guifg=#' .. color)
-            end
-
-            vim_item.kind = utils.lsp_icons.Tailwind
-            vim_item.kind_hl_group = group
-
-            return vim_item
-          end
-        end
-
-        vim_item.kind = utils.lsp_icons[vim_item.kind] or vim_item.kind
-
-        return vim_item
+        return utils.format_tailwind(entry, vim_item)
       end,
     }),
   },
