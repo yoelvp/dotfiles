25e4a956c27bc3656b55c2e4d058cbff18224912
yoelvp <yoelvp73@gmail.com>
2024-06-29 22:50:22 -0500

    feat: add new scripts

---
+++ Scripts/keyboard.sh
@@ -0,0 +1,4 @@
+sudo bash -c 'echo 7E2553 > /sys/devices/platform/hp-wmi/rgb_zones/zone00'
+sudo bash -c 'echo 7E2553 > /sys/devices/platform/hp-wmi/rgb_zones/zone01'
+sudo bash -c 'echo 7E2553 > /sys/devices/platform/hp-wmi/rgb_zones/zone02'
+sudo bash -c 'echo 7E2553 > /sys/devices/platform/hp-wmi/rgb_zones/zone03'
---
+++ Scripts/tmux.sh
@@ -0,0 +1,11 @@
+function tmx {
+  name=$(basename `pwd` | sed -e 's/\.//g')
+
+  if tmux ls 2>&1 | grep "$name"; then
+    tmux attach -t " $name"
+  elif [ -f .envrc ]; then
+    direnv exec / tmux new-session -s " $name"
+  else
+    tmux new-session -s " $name"
+  fi
+}
