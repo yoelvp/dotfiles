c9147838fc2ae766deeb39bb4fb25063efb57a40
yoelvp <yoelvp73@gmail.com>
2024-10-13 20:43:02 -0500

    fix: change config provider of linter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,24 +6,25 @@ return {
 
     conform.setup({
       formatters_by_ft = {
+        astro = { 'eslint_d', 'prettierd', 'eslint', 'prettier' },
         css = { 'stylelint' },
         go = { 'gofmt' },
-        javascript = { 'eslint_d' },
-        javascriptreact = { 'eslint_d' },
+        javascript = { 'eslint_d', 'eslint', 'prettier' },
+        javascriptreact = { 'eslint_d', 'eslint', 'prettier' },
         lua = { 'stylua' },
-        html = { 'prettierd' },
+        html = { 'prettierd', 'eslint_d', 'prettier' },
         rust = { 'rustfmt' },
-        typescript = { 'eslint_d' },
-        typescriptreact = { 'eslint_d' },
-      },
+        typescript = { 'eslint_d', 'prettierd', 'prettier' },
+        typescriptreact = { 'eslint_d', 'prettierd', 'prettier' }
+      }
     })
 
     -- Keymap for format document
     vim.keymap.set({ 'n', 'v' }, '<leader>fm', function()
       conform.format({
         lsp_callback = true,
-        async = false,
-        timeout_ms = 500,
+        async = true,
+        timeout_ms = 2500,
       })
     end, { noremap = true, silent = true, desc = 'Format file' })
   end,
