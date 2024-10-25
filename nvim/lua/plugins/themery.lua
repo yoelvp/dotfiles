341af54e74bda6d6b8853d33939820fb0de9e872
yoelvp <yoelvp73@gmail.com>
2024-10-24 23:07:16 -0500

    fix: remove unneceasry themes

--- nvim/lua/plugins/themery.lua
+++ nvim/lua/plugins/themery.lua
@@ -5,41 +5,8 @@ return {
     livePreview = true,
     themes = {
       {
-        name = 'Everforest Hard',
-        colorscheme = 'everforest',
-        after = [[
-          vim.cmd('let g:everforest_background = "hard"')
-        ]],
-      },
-      {
-        name = 'Everforest Medium',
-        colorscheme = 'everforest',
-        after = [[
-          vim.cmd('let g:everforest_background = "medium"')
-        ]],
-      },
-      {
-        name = 'Everforest Soft',
-        colorscheme = 'everforest',
-        after = [[
-          vim.cmd('let g:everforest_background = "soft"')
-        ]],
-      },
-      {
-        name = 'Gruvbox',
-        colorscheme = 'gruvbox',
-      },
-      {
-        name = 'Onedark Dark',
-        colorscheme = 'onedark_dark',
-      },
-      {
-        name = 'Onedark Vivid',
-        colorscheme = 'onedark_vivid',
-      },
-      {
-        name = 'Palenight',
-        colorscheme = 'palenight',
+        name = 'Gruvbox Material',
+        colorscheme = 'gruvbox-material',
       },
       {
         name = 'Solarized Osaka',
