0b441852ecfa4ec4da3111b9d068ff46f67402dc
yoelvp <yoelvp73@gmail.com>
2024-10-24 23:06:57 -0500

    fix: clean colorschemas

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,69 +1,15 @@
 return {
-  -- Gruvbox
+  -- Gruvbox material
   {
-    'ellisonleao/gruvbox.nvim',
+    'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000,
     config = function()
-      require('gruvbox').setup({
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = false,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = true,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = true,
-        inverse = true,
-        contrast = 'hard',
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-    end,
-  },
-
-  -- Onedark
-  {
-    'olimorris/onedarkpro.nvim',
-    priority = 1000,
-    config = function()
-      local onedark = require('onedarkpro')
-
-      onedark.setup({
-        styles = {
-          comments = 'italic',
-          keywords = 'bold,italic',
-          conditionals = 'italic',
-          virtual_text = 'italic',
-        },
-        highlights = {
-          Comment = { italic = true },
-        },
-        options = {
-          lualine_transparency = true,
-        },
-      })
-    end,
-  },
-
-  -- Everforest
-  {
-    'sainnhe/everforest',
-    priority = 1000,
-    config = true,
-  },
-
-  -- Palenight
-  {
-    'drewtempelmeyer/palenight.vim',
-    priority = 1000,
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_enable_italic = true
+    end
   },
 
   -- Solarized Osaka
