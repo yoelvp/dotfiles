06c09e0abd0c6adec800641f5595811b582e996a
yoelvp <yoelvp73@gmail.com>
2024-10-26 22:21:46 -0500

    feat: add colors support

---
+++ nvim/lua/plugins/colors.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'NvChad/nvim-colorizer.lua',
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+  {
+    'nvchad/volt',
+    lazy = true
+  },
+  {
+    'nvchad/minty',
+    cmd = { 'Shades', 'Huefy' }
+  }
+}
