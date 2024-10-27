3a5a5583cec14e3fc2a2a3c1933f30875a0eafc0
yoelvp <yoelvp73@gmail.com>
2024-10-26 22:21:36 -0500

    fix: add keys to extension

--- nvim/lua/plugins/codesnap.lua
+++ nvim/lua/plugins/codesnap.lua
@@ -1,13 +1,18 @@
 return {
   'mistricky/codesnap.nvim',
-  build = 'make',
+  build = 'make build_generator',
   version = '^1',
+  keys = {
+    { '<leader>cs', ':CodeSnap<CR>', mode = 'x', desc = 'Save screenshot to clipboard' },
+    { '<leader>cS', ':CodeSnapSave<CR>', mode = 'x', desc = 'Save the code capture locally' }
+  },
   opts = {
     mac_window_bar = true,
     title = 'CodeSnap.nvim',
     watermark = 'yoelvp',
     code_font_family = 'CaskaydiaMono Nerd Font',
-    bg_theme = 'peach',
+    bg_theme = 'dusk',
+    bg_padding = 16,
     save_path = '~/Desktop/Screenshots/Code',
   },
   config = function(_, opts)
