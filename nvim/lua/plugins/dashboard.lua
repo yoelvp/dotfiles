eb8df31e845b48afcaa97df7d7891caa60afa693
yoelvp <yoelvp73@gmail.com>
2024-10-26 22:22:00 -0500

    style: remove comments

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -24,8 +24,6 @@ return {
           { action = 'ene | startinsert', desc = ' New file', icon = ' ', key = 'n' },
           { action = 'Telescope oldfiles', desc = ' Recent files', icon = ' ', key = 'r' },
           { action = 'Telescope live_grep', desc = ' Find text', icon = ' ', key = 'g' },
-          --[[ { action = 'lua require("persistence").load()', desc = ' Restore Session', icon = ' ', key = 's' }, ]]
-          --[[ { action = 'LazyExtras', desc = ' Lazy Extras', icon = ' ', key = 'x' }, ]]
           { action = 'Lazy', desc = ' Lazy', icon = '󰒲 ', key = 'l' },
           { action = 'qa', desc = ' Quit', icon = ' ', key = 'q' },
         },
