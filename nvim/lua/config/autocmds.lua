75d9628a37a457aa2d7f6b7c209c8560305d5223
yoelvp <yoelvp73@gmail.com>
2025-03-03 11:52:01 -0500

    fix: clean cmds

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,26 +12,6 @@ autocmd('BufEnter', {
   end,
 })
 
--- Go to last loc when opening a buffer
-autocmd('BufReadPost', {
-  group = augroup('last_loc'),
-  callback = function(event)
-    local exclude = { 'gitcommit' }
-    local buf = event.buf
-
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].yoelvp_last_loc then
-      return
-    end
-
-    vim.b[buf].yoelvp_last_loc = true
-    local mark = vim.api.nvim_buf_get_mark(buf, '"')
-    local lcount = vim.api.nvim_buf_line_count(buf)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
 -- Highlight the copied block
 autocmd('TextYankPost', {
   pattern = '*',
@@ -46,23 +26,3 @@ vim.cmd([[
   let &t_Cs = "\e[4:3m"
   let &t_Ce = "\e[4:0m"
 ]])
-
--- Use templates in files
-autocmd('BufNewFile', {
-  pattern = '*',
-  callback = function()
-    local extension = vim.fn.expand('%:e')
-    local template = vim.env.HOME .. '/.config/nvim/templates/' .. extension .. '.tpl'
-    local file = io.open(template, 'r')
-
-    if file ~= nil then
-      local lines = {}
-
-      for line in io.lines(template) do
-        lines[#lines + 1] = line
-      end
-
-      vim.api.nvim_buf_set_lines(0, 0, 0, false, lines)
-    end
-  end,
-})
