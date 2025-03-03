0f96f22c4bf1321edf951b00c69ced9287c015fc
yoelvp <yoelvp73@gmail.com>
2025-03-03 11:53:14 -0500

    fix: remove unused functions

--- nvim/lua/yoelvp/utils/init.lua
+++ nvim/lua/yoelvp/utils/init.lua
@@ -22,30 +22,6 @@ function M.duplicate_line_above()
   vim.fn.cursor(vim.fn.line('.'), col)
 end
 
--- Duplicate a code block below
-function M.duplicate_block_below()
-  local start_line, start_col = vim.fn.getpos("'<")[2], vim.fn.getpos("'<")[3]
-  local end_line, end_col = vim.fn.getpos("'>")[2], vim.fn.getpos("'>")[3]
-
-  vim.api.nvim_command('normal! gv"zy')
-  vim.api.nvim_command('normal! `>p')
-
-  vim.fn.setpos("'<", { 0, start_line, start_col, 0 })
-  vim.fn.setpos("'>", { 0, end_line, end_col, 0 })
-end
-
--- Duplicate a code block above
-function M.duplicate_block_above()
-  local start_line, start_col = vim.fn.getpos("'<")[2], vim.fn.getpos("'<")[3]
-  local end_line, end_col = vim.fn.getpos("'>")[2], vim.fn.getpos("'>")[3]
-
-  vim.api.nvim_command('normal! gv"zy')
-  vim.api.nvim_command('normal! `>P')
-
-  vim.fn.setpos("'<", { 0, start_line, start_col, 0 })
-  vim.fn.setpos("'>", { 0, end_line, end_col, 0 })
-end
-
 -- Remove the current buffer
 function M.delete_current_buffer()
   local bufnr = vim.api.nvim_get_current_buf()
@@ -55,32 +31,6 @@ end
 -- TODO: Seleccionar todo el contenido y regresar el cursor a su lugar donde estaba
 function M.select_all_content()
   print('Haz hecho click')
-end
-
-function M.format_tailwind(entry, vim_item)
-  if vim_item.kind == 'Color' and entry.completion_item.documentation then
-    local _, _, r, g, b =
-      ---@diagnostic disable-next-line: param-type-mismatch
-      string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
-
-    if r and g and b then
-      local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
-      local group = 'Tw_' .. color
-
-      if vim.api.nvim_call_function('hlID', { group }) < 1 then
-        vim.api.nvim_command('highlight' .. ' ' .. group .. ' ' .. 'guifg=#' .. color)
-      end
-
-      vim_item.kind = M.lsp_icons.Tailwind
-      vim_item.kind_hl_group = group
-
-      return vim_item
-    end
-  end
-
-  vim_item.kind = M.lsp_icons[vim_item.kind] or vim_item.kind
-
-  return vim_item
 end
 
 return M
