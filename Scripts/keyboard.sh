02eb3f8937520f7bea932a10059d4ba0bfcc9652
yoelvp <yoelvp73@gmail.com>
2025-06-18 16:23:16 -0500

    fix: change keyboard color

--- Scripts/keyboard.sh
+++ Scripts/keyboard.sh
@@ -1,4 +1,4 @@
-sudo bash -c 'echo 7E2553 > /sys/devices/platform/hp-wmi/rgb_zones/zone00'
-sudo bash -c 'echo 7E2553 > /sys/devices/platform/hp-wmi/rgb_zones/zone01'
-sudo bash -c 'echo 7E2553 > /sys/devices/platform/hp-wmi/rgb_zones/zone02'
-sudo bash -c 'echo 7E2553 > /sys/devices/platform/hp-wmi/rgb_zones/zone03'
+sudo bash -c 'echo 115F5D > /sys/devices/platform/hp-wmi/rgb_zones/zone00'
+sudo bash -c 'echo 115F5D > /sys/devices/platform/hp-wmi/rgb_zones/zone01'
+sudo bash -c 'echo 115F5D > /sys/devices/platform/hp-wmi/rgb_zones/zone02'
+sudo bash -c 'echo 115F5D > /sys/devices/platform/hp-wmi/rgb_zones/zone03'
