deeeb043b69843867c61fe1c36ecd208c4c8030e
yoelvp <yoelvp73@gmail.com>
2026-01-22 18:54:11 -0500

    fix: update

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -1,35 +1,35 @@
 local opt = vim.opt
 
-vim.g.mapleader = ' '
-vim.g.snippets = 'luasnip'
+vim.g.mapleader = " "
+vim.g.snippets = "luasnip"
 
-opt.syntax = 'on'
+-- Appearance
+opt.syntax = "on"
 opt.title = true
-opt.background = 'dark'
+opt.background = "dark"
 opt.termguicolors = true
 opt.cursorline = true
-opt.colorcolumn = '120'
-opt.signcolumn = 'yes'
-
--- encoding
-opt.encoding = 'utf-8'
-vim.scriptencoding = 'utf-8'
-opt.fileencoding = 'utf-8'
-
+opt.colorcolumn = "120"
+opt.signcolumn = "yes"
 opt.number = true
-opt.relativenumber = true
 opt.numberwidth = 2
+opt.relativenumber = true
 opt.wrap = false
-
 opt.scrolloff = 5
 opt.sidescrolloff = 5
-opt.list = true
-opt.listchars = 'tab:▸ ,space:·,nbsp:␣,trail:•,precedes:«,extends:»'
-opt.linespace = 0
-opt.hlsearch = false
+
+-- encoding
+opt.encoding = "utf-8"
+vim.scriptencoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+-- Search
+opt.hlsearch = true
 opt.incsearch = true
 opt.ignorecase = true
 opt.smartcase = true
+
+-- Splits
 opt.splitbelow = true
 opt.splitright = true
 
@@ -41,19 +41,22 @@ opt.shiftwidth = 2
 opt.tabstop = 2
 opt.softtabstop = 2
 
-opt.clipboard = 'unnamedplus'
-opt.mouse = 'a'
-opt.updatetime = 100
-
 -- swapfiles
 opt.hidden = true
 opt.swapfile = false
 opt.synmaxcol = 1000
 
 -- Fold column
-opt.foldcolumn = '0'
+opt.foldcolumn = "0"
 opt.foldenable = false
-opt.foldmethod = 'indent'
+opt.foldmethod = "indent"
 opt.foldlevel = 99
 opt.foldlevelstart = 99
 opt.foldminlines = 2
+
+opt.clipboard = "unnamedplus"
+opt.mouse = "a"
+opt.updatetime = 100
+opt.list = true
+opt.listchars = "tab:▸ ,space:·,nbsp:␣,trail:•,precedes:«,extends:»"
+opt.linespace = 0
